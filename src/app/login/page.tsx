29c839addd7991182f031fb8c1eb69d4e57e0bc8
sanish <mhrznsanish@gmail.com>
2025-01-16 16:40:27 +1000

    ui changes to landing page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,16 +6,17 @@ function page() {
   return (
     <main className="flex min-h-screen flex-col items-center ">
       <div className="flex flex-col md:flex-row justify-around w-full">
-     
-        <div className="p-10 sm:p-0 flex-1 md:ms-32 flex justify-center items-center ">
+
+        <div className="p-10 sm:p-0 flex-1 md:ms-32 flex justify-center items-center flex-col gap-4">
           <Image 
             src={"/landing.png"} 
-            height={750} 
-            width={750} 
-            style={{objectFit:"contain", marginTop:"6%"}} 
+            height={600} 
+            width={600} 
+            style={{objectFit:"contain", marginTop:"4%"}} 
             alt="landing image of files" 
             className="max-w-full"
           />
+          <h1 className='text-xl mx-32 mt-10 text-center font-semibold'>Seamless Collaboration, Effortless Editing, and Hassle-Free PDF Signing – All in One Powerful Platform!</h1>
         </div>
         <div className="flex-1 flex justify-center">
           <LoginForm />
